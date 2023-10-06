f2b2a0104747c546e964f8321b9bfd140f92bd00
Dmitry Conquer <dmitry.conquer@gmail.com>
2023-10-06 14:40:32 +0300

    прибрана валідація номерів

--- src/js/modules/formValidate.js
+++ src/js/modules/formValidate.js
@@ -1,4 +1,3 @@
-import IMask from "imask";
 const telField = document.getElementById("tel");
 const nameField = document.getElementById("firstName");
 const form = document.getElementById("form");
@@ -6,13 +5,6 @@ const nameError = document.getElementById("name-error");
 const telError = document.getElementById("tel-error");
 
 form.addEventListener("focusin", e => {
-  if (e.target.closest("#tel")) {
-    const maskOptions = {
-      mask: "+{38}(000)000-00-00",
-      lazy: false,
-    };
-    IMask(telField, maskOptions);
-  }
   if (e.target.closest("#tel") || e.target.closest("#firstName")) {
     nameError.classList.add("hidden");
     telError.classList.add("hidden");
@@ -20,20 +12,14 @@ form.addEventListener("focusin", e => {
 });
 
 form.addEventListener("submit", e => {
-  if (!telTest(telField.value)) {
-     telError.classList.remove("hidden");
-    telError.classList.add("visible");
-    e.preventDefault();
-    console.log("error!");
-  }
   if (nameField.value === "") {
-     nameError.classList.remove("hidden");
+    nameError.classList.remove("hidden");
     nameError.classList.add("visible");
     e.preventDefault();
-    console.log("error!");
+  }
+  if (telField.value === "") {
+    telError.classList.remove("hidden");
+    telError.classList.add("visible");
+    e.preventDefault();
   }
 });
-
-function telTest(tel) {
-  return /^[\+]?3?[\s]?8?[\s]?\(?0\d{2}?\)?[\s]?\d{3}[\s|-]?\d{2}[\s|-]?\d{2}$/.test(tel);
-}
